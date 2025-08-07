574278abf565b2a266c30293528516750f97f522
shivanswamynathan <46991694+shivanswamynathan@users.noreply.github.com>
2025-08-07 09:15:33 +0530

    Update reconciliation_urls.py

--- reconciliation/document_processing/urls/reconciliation_urls.py
+++ reconciliation/document_processing/urls/reconciliation_urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-from ..views import invoice_recon_views, match_views
+
+from ..views.reconciliation import match_views
+from ..views.reconciliation import invoice_recon_views
+from ..views.reconciliation import manual_match_views
 
 urlpatterns = [
     path("reconciliation/",
@@ -7,5 +10,7 @@ urlpatterns = [
     path("status-approval/",
          match_views.ReconciliationApprovalAPI.as_view(), name="status_approval"),
     path("reconciliation-match/",
-          match_views.ReconciliationDetailAPI.as_view(), name='reconciliation_match_detail'),     
+         match_views.ReconciliationDetailAPI.as_view(), name='reconciliation_match_detail'),
+    path("manual-match/",
+         manual_match_views.ManualMatchAPI.as_view(), name="manual_match"),
 ]
