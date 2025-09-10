4c3022c6eef9d22ac0a20a92ed2760bdb3bd7240
shivanswamynathan <46991694+shivanswamynathan@users.noreply.github.com>
2025-09-10 18:40:08 +0530

    Update invoice_urls.py

--- reconciliation/document_processing/urls/invoice_urls.py
+++ reconciliation/document_processing/urls/invoice_urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
-from ..views import views, manually_enter_views, upload_key_views, invoice_views
+
+from ..views.match import invoice_views, manually_enter_views
+
+from ..views.sync import upload_key_views
+from ..views.match import views
 
 urlpatterns = [
     path("process-invoice/", views.ProcessInvoiceAPI.as_view(),
@@ -13,4 +17,16 @@ urlpatterns = [
     path("missing-invoices/",
          invoice_views.MissingInvoiceListAPI.as_view(), name="missing_invoices"),
     path("ocr-issues/", invoice_views.OCRIssuesListAPI.as_view(), name="ocr_issues"),
+
+    path("boe-extractions/",
+         invoice_views.BoeExtractionAPI.as_view(), name="boe_extractions"),
+
+    path("fetch-data/",
+          invoice_views.FetchDataAPI.as_view(), name="fetch_data"),
+    path("duplicate-invoices/",
+         invoice_views.DuplicateInvoiceAPI.as_view(), name="duplicate_invoices"),
+    path("duplicate-invoice-flag/",invoice_views.DuplicateInvoiceFlagAPI.as_view(), name="duplicate_invoice_flag"),
+
+    path("duplicate-issue/",
+          invoice_views.DuplicateIssuesAPI.as_view(), name="duplicate_issue"),
 ]
