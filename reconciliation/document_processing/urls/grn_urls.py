745ab67353bbf0d0ae11027e4c69a71bb542665b
shivanswamynathan <46991694+shivanswamynathan@users.noreply.github.com>
2025-09-10 18:39:46 +0530

    Update grn_urls.py

--- reconciliation/document_processing/urls/grn_urls.py
+++ reconciliation/document_processing/urls/grn_urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from ..views import po_grn_views, itemwise_grn_views
+
+from ..views.upload import po_grn_views
+from ..views.upload import itemwise_grn_views
+from ..views.upload import missing_data_checkere
 
 urlpatterns = [
     path("process-po-grn/", po_grn_views.ProcessPoGrnAPI.as_view(),
          name="process_po_grn"),
     path("process-itemwise-grn/",
          itemwise_grn_views.ProcessItemWiseGrnAPI.as_view(), name="process_itemwise_grn"),
+     path("check-missing-data/",missing_data_checkere.CheckMissingDataAPI.as_view(),
+         name="check_missing_data"),
 ]
